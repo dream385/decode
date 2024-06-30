6927a1d9bb9655f11c7efe7c82baeca86cdfcf6d
dream385 <165793513+dream385@users.noreply.github.com>
2024-06-30 09:04:17 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -59,7 +59,7 @@ for (let plugin of plugins) {
 let time = new Date();
 if (processedCode !== sourceCode) {
   // 输出代码
-  fs.writeFile(decodeFile, "//" + time + '\n' + "//Base:https://github.com/echo094/decode-js" + '\n' + "//Modify:https://github.com/dream385/Decode" + '\n' + processedCode, (err) => {
+  fs.writeFile(decodeFile, "//" + time + '\n' + "//Base:https://github.com/echo094/decode-js" + '\n' + "//Modify:https://github.com/dream385/decode" + '\n' + processedCode, (err) => {
     if (err) throw err;
     console.log(`使用插件 ${pluginUsed} 成功处理并写入文件 ${decodeFile}`);
   });
